5a176e7a8e5ff43f987cf85f772e8c6cf03a9f03
Zahira Douki <zahiradk31@gmail.com>
2026-05-24 23:21:28 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+const themeBtn = document.getElementById("themeBtn");
+
+themeBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+
+  if (document.body.classList.contains("dark")) {
+    themeBtn.textContent = "☀️";
+  } else {
+    themeBtn.textContent = "🌙";
+  }
+});
